0c99b729a868e085daa7e4e2cb5d087b6eb9a0fc
Manish Jangid <manishjangidmakrana@gmail.com>
2026-05-12 17:45:28 +0530

    add new change

--- src/components/home/AchievementHighlights.jsx
+++ src/components/home/AchievementHighlights.jsx
@@ -88,7 +88,7 @@ export default function AchievementHighlights() {
         },
       },
       {
-        breakpoint: 390,
+        breakpoint: 500,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
@@ -190,7 +190,7 @@ export default function AchievementHighlights() {
               {gallery.map((item, i) => (
                 <div
                   key={i}
-                  className="outline-none px-1 w-full lg:max-w-[560px] md:max-w-[400px] sm:max-w-[360px] xs:max-w-[280px] max-w-[260px]"
+                  className="outline-none px-1 w-full lg:max-w-[560px] md:max-w-[400px] sm:max-w-[360px] xs:max-w-[260px] max-w-[224px]"
                 >
                   <div className="rounded-lg overflow-hidden aspect-4/3 bg-gray-200 shadow-sm">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
